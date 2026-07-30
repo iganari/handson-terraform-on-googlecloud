03060ca4089e2150ee073347a8b15cc1cbce76e3
iganari / Toru Igarashi <cigaguri@gmail.com>
2026-07-31 07:37:07 +0900

    Update main.tf

--- scenario1/main.tf
+++ scenario1/main.tf
@@ -8,8 +8,7 @@ locals {
 resource "google_project_service" "scenario1_api" {
   for_each           = local.scenario1_services
   service            = each.value
-  # destroy時にAPIまで無効化すると他の影響が出やすいためfalseを推奨
-  disable_on_destroy = false 
+  disable_on_destroy = false
 }
 
 # 1. カスタムVPCの作成
@@ -17,7 +16,6 @@ resource "google_compute_network" "vpc_network" {
   name                    = "handson-vpc"
   auto_create_subnetworks = false
 
-  # APIが有効になってから作成を開始する
   depends_on = [google_project_service.scenario1_api]
 }
 
@@ -36,16 +34,28 @@ resource "google_compute_router" "router" {
   network = google_compute_network.vpc_network.id
 }
 
-# 4. Cloud NAT
+# 4. Cloud NAT用の静的外部IPアドレスを事前作成
+resource "google_compute_address" "nat_ip" {
+  name   = "handson-nat-ip"
+  region = google_compute_subnetwork.subnet.region
+
+  depends_on = [google_project_service.scenario1_api]
+}
+
+# 5. Cloud NAT (事前に作成した静的IPを固定割り当て)
 resource "google_compute_router_nat" "nat" {
   name                               = "handson-nat"
   router                             = google_compute_router.router.name
   region                             = google_compute_router.router.region
-  nat_ip_allocate_option             = "AUTO_ONLY"
+  
+  # 手動割り当てに変更し、作成したIPを指定
+  nat_ip_allocate_option             = "MANUAL_ONLY"
+  nat_ips                            = [google_compute_address.nat_ip.self_link]
+  
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
 
-# 5. ファイアウォールルール
+# 6. ファイアウォールルール (IAPからのSSHアクセスのみ許可)
 resource "google_compute_firewall" "allow_iap_ssh" {
   name    = "allow-iap-ssh"
   network = google_compute_network.vpc_network.name
@@ -57,7 +67,7 @@ resource "google_compute_firewall" "allow_iap_ssh" {
   source_ranges = ["35.235.240.0/20"]
 }
 
-# 6. Compute Engine (VM)
+# 7. Compute Engine (VM)
 resource "google_compute_instance" "web_server" {
   name         = "handson-web-server"
   machine_type = "e2-micro"
@@ -65,15 +75,18 @@ resource "google_compute_instance" "web_server" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      # Ops Agentがサポートする最新のDebian 12を指定
+      image = "debian-cloud/debian-12"
     }
   }
 
   network_interface {
     network    = google_compute_network.vpc_network.id
     subnetwork = google_compute_subnetwork.subnet.id
+    # 外部IPは付与しない
   }
 
+  # 起動時にNginxをインストール
   metadata_startup_script = <<-EOF
     #!/bin/bash
     apt-get update
